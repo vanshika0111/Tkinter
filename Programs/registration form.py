697a32f4b4eb9f7bde7aa1b88446a95d105fe0a1
Vanshika <vanshikag0111@gmail.com>
2021-09-21 14:03:44 +0530

    simplest registration form

---
+++ Programs/registration form.py
@@ -0,0 +1,36 @@
+# registration form
+
+from tkinter import *
+
+root = Tk()
+root.title("Registration Form")
+root.geometry("444x555")
+
+file = open("data.txt","a")
+def Values():
+    file.write(f"First name is {FirstName.get()}")
+    file.write(f"Last name is {LastName.get()}")
+    file.write(f"Phone number: {Phone.get()}")
+
+# f1 = Frame(root, bg="gray", borderwidth=6, relief=SUNKEN)
+# f1.pack(side=TOP, fill=X)
+# l = Label(f1, text="Registration Form")
+# l.pack(padx=143)
+
+Label(root, text="First Name:").grid(row=1,column=0)
+Label(root, text="Last Name:").grid(row=2,column=0)
+Label(root, text="Phone Number:").grid(row=3,column=0)
+
+FirstName = StringVar()
+LastName = StringVar()
+Phone = IntVar()
+
+Entry(root, textvariable = FirstName).grid(row=1, column=1)
+Entry(root, textvariable = LastName).grid(row=2, column=1)
+Entry(root, textvariable = Phone).grid(row=3, column=1)
+
+Button(root, fg="blue", text="Register", command=Values).grid(row=4,column=0)
+
+file.close()
+
+root.mainloop()
