f27aca24f5a6e5214c2fdfe2db5f888058c4a816
Vanshika <vanshikag0111@gmail.com>
2021-09-21 13:59:43 +0530

    display images in GUI

---
+++ Concepts/displaypic.py
@@ -0,0 +1,28 @@
+# display image in GUI
+
+from tkinter import *
+
+#imprt the following module to display jpg images
+from PIL import Image, ImageTk
+import os
+
+#creates basic gui  #Tk is an object
+root = Tk()  
+
+#to set the size of window ("widthxheight")
+root.geometry("320x160")
+
+#to display image in the window with extension of png
+# photo = PhotoImage(file="1.png")
+# label = Label(image=photo)
+# label.pack()
+
+#to display image in the window with extension jpg
+image = Image.open("vanshika.jpg")
+photo2 = ImageTk.PhotoImage(image)
+label = Label(image=photo2)
+label.pack()
+
+#creates event loop 
+root.mainloop()   
+
